983651de53ac985cac8484dc028a3fd425a440ba
Maya McKinney <mayamckinney0306@gmail.com>
2023-02-24 12:28:40 -0700

    user model

---
+++ models/user.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose');
+
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: [validateEmail, 'Please fill a valid email address'],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']    
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        // set the virtuals and getters for user accts
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+const User = model('user', userSchema);
+
+// allows to grab friends & show how many
+userSchema.virtual('friends').get(function () {
+    return this.friends.length;
+});
+
+module.exports = User;
